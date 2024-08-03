a735c6b3f5e8822d381c0940737cd9fe9df7f7fc
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-08-03 19:17:28 +0700

    feat: redirect to /dashboard if there is token

--- src/app/(auth-wrapper)/sign-in/page.tsx
+++ src/app/(auth-wrapper)/sign-in/page.tsx
@@ -1,7 +1,19 @@
+// src/app/sign-in/page.tsx
 import React from "react";
 import SignInView from "@/components/sections/sign-in/SignInView";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const SignIn = () => {
+const SignIn = async () => {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value || null;
+
+  if (token) {
+    // Perform token validation if necessary
+    // If valid, redirect to another page
+    redirect("/dashboard");
+  }
+
   return <SignInView />;
 };
 
