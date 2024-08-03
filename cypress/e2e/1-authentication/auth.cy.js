db203863f68ab4c78222517fa664be0b0b5089c5
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-08-04 05:07:45 +0700

    feat: add cypress and login test

---
+++ cypress/e2e/1-authentication/auth.cy.js
@@ -0,0 +1,38 @@
+describe("Login Test", () => {
+  it("should login with valid credentials", () => {
+    // Visit the login page
+    cy.visit("http://localhost:3000/sign-in");
+
+    // Enter email
+    cy.get('input[name="email"]').type("administrator@asha.com");
+
+    // Enter password
+    cy.get('input[name="password"]').type("secret"); // Replace 'yourPassword' with the actual password
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+
+    // Assert the user is redirected to the dashboard
+    cy.url().should("include", "/dashboard");
+
+    // Assert the presence of a dashboard element
+    cy.contains("Dashboard").should("be.visible");
+  });
+
+  it("should not login with invalid credentials", () => {
+    // Visit the login page
+    cy.visit("http://localhost:3000/sign-in");
+
+    // Enter wrong email
+    cy.get('input[name="email"]').type("administrator123@asha.com");
+
+    // Enter wrong password
+    cy.get('input[name="password"]').type("secret123");
+
+    // Click login button
+    cy.get('button[type="submit"]').click();
+
+    // Assert the user is not redirected to the dashboard
+    cy.url().should("not.include", "/dashboard");
+  });
+});
