f970814cfcfe3c4b9846feeab23895ae16e14b65
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-08-03 20:41:45 +0700

    fix: update schema database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,19 +3,61 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
   id: integer("id").primaryKey(),
-  name: text("name").notNull(),
-  age: integer("age").notNull(),
+  fullname: text("fullname").notNull(),
   email: text("email").unique().notNull(),
+  role: text("role", { enum: ["admin", "manager", "user"] }),
+  status: text("status", { enum: ["active", "inactive"] }),
   password: text("password").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date()
+  ),
 });
 
-export const postsTable = sqliteTable("posts", {
+export const categoriesTable = sqliteTable("categories", {
   id: integer("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
+  name: text("name").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date()
+  ),
+});
+
+export const productsTable = sqliteTable("products", {
+  id: integer("id").primaryKey(),
+  categoryId: integer("category_id")
+    .notNull()
+    .references(() => categoriesTable.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  color: text("color").notNull(),
+  description: text("description").notNull(),
+  price: integer("price").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date()
+  ),
+});
+
+export const ordersTable = sqliteTable("orders", {
+  id: integer("id").primaryKey(),
   userId: integer("user_id")
     .notNull()
     .references(() => usersTable.id, { onDelete: "cascade" }),
+  productId: integer("product_id")
+    .notNull()
+    .references(() => productsTable.id, { onDelete: "cascade" }),
+  orderDate: text("order_date").notNull(),
+  status: text("status", { enum: ["pending", "shipped", "delivered"] }),
+  paymentMethod: text("payment_method").notNull(),
+  shippingAddress: text("shipping_address").notNull(),
+  quantity: integer("quantity").notNull(),
+  totalAmount: integer("total_amount").notNull(),
   createdAt: text("created_at")
     .default(sql`(CURRENT_TIMESTAMP)`)
     .notNull(),
@@ -27,5 +69,11 @@ export const postsTable = sqliteTable("posts", {
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertCategory = typeof categoriesTable.$inferInsert;
+export type SelectCategory = typeof categoriesTable.$inferSelect;
+
+export type InsertProduct = typeof productsTable.$inferInsert;
+export type SelectProduct = typeof productsTable.$inferSelect;
+
+export type InsertOrder = typeof ordersTable.$inferInsert;
+export type SelectOrder = typeof ordersTable.$inferSelect;
