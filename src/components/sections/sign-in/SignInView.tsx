de3bff0a12413938abdfc0c3b03e10b434109972
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2024-08-05 13:13:56 +0700

    feat: handle animate component

--- src/components/sections/sign-in/SignInView.tsx
+++ src/components/sections/sign-in/SignInView.tsx
@@ -23,8 +23,9 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useForm } from "react-hook-form";
+import { AnimatePage } from "@/components/shared/animate-page";
 
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import signInSchema from "@/lib/zod/sign-in.schema";
 
@@ -68,70 +69,76 @@ const SignInView = () => {
     <div className="w-full h-[100vh] flex items-center justify-center bg-primary">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle className="text-2xl">Asha ShopEase</CardTitle>
-              <CardDescription>
-                Enter your email below to login to your account.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="email">Email</Label>
-                      <FormControl>
-                        <Input
-                          id="email"
-                          type="email"
-                          placeholder="Enter Email Address"
-                          required
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="password">Password</Label>
-                      <FormControl>
-                        <Input
-                          id="password"
-                          type="password"
-                          placeholder="Enter Password"
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <FormMessage />
-            </CardContent>
-            <CardFooter>
-              <div className="w-full">
-                <Button type="submit" className="w-full" isLoading={isPending}>
-                  Sign in
-                </Button>
-
-                <div className="mt-4 text-center text-sm">
-                  Don&apos;t have an account?{" "}
-                  <Link href="/sign-up" className="underline">
-                    Sign up
-                  </Link>
+          <AnimatePage>
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle className="text-2xl">Asha ShopEase</CardTitle>
+                <CardDescription>
+                  Enter your email below to login to your account.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="email">Email</Label>
+                        <FormControl>
+                          <Input
+                            id="email"
+                            type="email"
+                            placeholder="Enter Email Address"
+                            required
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
                 </div>
-              </div>
-            </CardFooter>
-          </Card>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="password">Password</Label>
+                        <FormControl>
+                          <Input
+                            id="password"
+                            type="password"
+                            placeholder="Enter Password"
+                            {...field}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormMessage />
+              </CardContent>
+              <CardFooter>
+                <div className="w-full">
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    isLoading={isPending}
+                  >
+                    Sign in
+                  </Button>
+
+                  <div className="mt-4 text-center text-sm">
+                    Don&apos;t have an account?{" "}
+                    <Link href="/sign-up" className="underline">
+                      Sign up
+                    </Link>
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </AnimatePage>
         </form>
       </Form>
     </div>
--- src/components/sections/sign-in/SignUpView.tsx
+++ src/components/sections/sign-in/SignUpView.tsx
@@ -24,12 +24,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useForm } from "react-hook-form";
+import { AnimatePage } from "@/components/shared/animate-page";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import signInSchema from "@/lib/zod/sign-in.schema";
 
 import { z } from "zod";
-import { useLogin, useRegister } from "@/hooks/api/useAuth";
+import { useRegister } from "@/hooks/api/useAuth";
 import { useRouter } from "next/navigation";
 
 import { useToast } from "@/components/ui/use-toast";
@@ -77,114 +77,122 @@ const SignInView = () => {
     <div className="w-full h-[100vh] flex items-center justify-center bg-primary">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
-          <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle className="text-2xl">Asha ShopEase</CardTitle>
-              <CardDescription>
-                Create a new account by entering your details below
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="fullname"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="fullname">Fullname</Label>
-                      <FormControl>
-                        <Input
-                          id="fullname"
-                          type="text"
-                          placeholder="Enter Your Fullname"
-                          required
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="email">Email</Label>
-                      <FormControl>
-                        <Input
-                          id="email"
-                          type="email"
-                          placeholder="Enter Email Address"
-                          required
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="password">Password</Label>
-                      <FormControl>
-                        <Input
-                          id="password"
-                          type="password"
-                          placeholder="Enter Password"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="confirmPassword"
-                  render={({ field }) => (
-                    <FormItem>
-                      <Label htmlFor="confirmPassword">Confirm Password</Label>
-                      <FormControl>
-                        <Input
-                          id="confirmPassword"
-                          type="password"
-                          placeholder="Enter Confirm Password"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <div className="w-full">
-                <Button type="submit" className="w-full" isLoading={isPending}>
-                  Sign up
-                </Button>
-
-                <div className="mt-4 text-center text-sm">
-                  Already have an account?{" "}
-                  <Link href="/sign-in" className="underline">
-                    Sign in
-                  </Link>
+          <AnimatePage>
+            <Card className="w-full max-w-sm">
+              <CardHeader>
+                <CardTitle className="text-2xl">Asha ShopEase</CardTitle>
+                <CardDescription>
+                  Create a new account by entering your details below
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="fullname"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="fullname">Fullname</Label>
+                        <FormControl>
+                          <Input
+                            id="fullname"
+                            type="text"
+                            placeholder="Enter Your Fullname"
+                            required
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-              </div>
-            </CardFooter>
-          </Card>
+
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="email">Email</Label>
+                        <FormControl>
+                          <Input
+                            id="email"
+                            type="email"
+                            placeholder="Enter Email Address"
+                            required
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="password">Password</Label>
+                        <FormControl>
+                          <Input
+                            id="password"
+                            type="password"
+                            placeholder="Enter Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <Label htmlFor="confirmPassword">
+                          Confirm Password
+                        </Label>
+                        <FormControl>
+                          <Input
+                            id="confirmPassword"
+                            type="password"
+                            placeholder="Enter Confirm Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div className="w-full">
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    isLoading={isPending}
+                  >
+                    Sign up
+                  </Button>
+
+                  <div className="mt-4 text-center text-sm">
+                    Already have an account?{" "}
+                    <Link href="/sign-in" className="underline">
+                      Sign in
+                    </Link>
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </AnimatePage>
         </form>
       </Form>
     </div>
